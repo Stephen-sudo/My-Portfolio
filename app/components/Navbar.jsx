9ad01761ce1fe87a1f1833c636eb8f728e0944b8
Stephen-sudoa <nanagyasi180@gmail.com>
2025-06-07 23:43:33 +0100

    image change

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = ({ isDarkMode, setIsDarkMode }) => {
       </div>
 
       <nav
-        className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20" : ""}`}
+        className={`w-full  fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20" : ""}`}
       >
         <a href="#top">
           <Image
